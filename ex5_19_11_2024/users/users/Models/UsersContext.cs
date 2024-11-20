daae1873246140670187d7c9bb03afaf0faa7f2b
AdinaManes <327581831@mby.co.il>
2024-11-20 02:15:14 +0200

    add file to git

---
+++ ex5_19_11_2024/users/users/Models/UsersContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace users.Models;
+
+public partial class UsersContext : DbContext
+{
+    public UsersContext()
+    {
+    }
+
+    public UsersContext(DbContextOptions<UsersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Attachment> Attachments { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+    public virtual DbSet<Project> Projects { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+    public object Project { get; internal set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Attachment>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.DateUplode).HasColumnName("dateUplode");
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.Path)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("path");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ex5_19_11_2024/users/users/Repositoreis/TasksRepositoreis.cs
@@ -0,0 +1,20 @@
+﻿using users.Models;
+
+namespace users.Repositoreis
+{
+    public class TasksRepositoreis : ITasksRepositoreis
+    {
+        private readonly UsersContext _context;
+
+        public TasksRepositoreis(UsersContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Models.Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+    }
+}
